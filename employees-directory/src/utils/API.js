5d438bb4e536b2bbe8327056319fe42155b7eb2c
Greyson Mainor <gbmainor98@outlook.com>
2021-05-01 19:49:47 -0400

    added API.js.

---
+++ employees-directory/src/utils/API.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    // Retrieve 20 random users
+    search: function () {
+        return axios.get('https://randomuser.me/api/?results=20&nat=us');
+    },
+};
