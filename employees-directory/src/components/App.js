b70ebea7472c694f4c380aed5eb51ffb85cd1331
Greyson Mainor <gbmainor98@outlook.com>
2021-05-01 20:10:39 -0400

    refactored App.js and index.js.

--- employees-directory/src/components/App.js
+++ employees-directory/src/components/App.js
@@ -1,13 +1,34 @@
-import React from "react";
-import Signup from "./pages/Signup";
-import "./App.css";
+import React from 'react';
+import EmployeeCard from './components/EmployeeCard';
+import Wrapper from './components/Wrapper';
+import API from './utils/API';
+import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Signup />
-    </div>
-  );
-}
+class App extends React.Component {
+  state = { employees: [] };
 
-export default App;
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = (query) => {
+    API.search(query)
+      .then((res) => this.setState({ result: res.data }))
+      .catch((err) => console.log(err));
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        {/* Looping through all the users from the API  */}
+        {[...this.state.result].map((item) => (
+          <EmployeeCard
+            picture={item.picture}
+            firstName={item.firstName}
+            lastName={item.lastName}
+            email={item.email}
+            phone={item.phone}
+            key={item.key}
+          />
+        ))}
+      </Wrapper>
