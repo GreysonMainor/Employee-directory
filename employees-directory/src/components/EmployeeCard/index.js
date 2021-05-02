94119c381f2ef3e23369b0a33cdc5586b30439e7
Greyson Mainor <gbmainor98@outlook.com>
2021-05-01 20:16:03 -0400

    refactored index.js.

--- employees-directory/src/components/EmployeeCard/index.js
+++ employees-directory/src/components/EmployeeCard/index.js
@@ -1,45 +1,19 @@
 import React from 'react';
-import './style.css';
 
 function EmployeeCard(props) {
   return (
-    <div className="card">
-      <div className="content">
-        <table className="data-area">
-          <thead className="table-header">
-            <tr>
-              <th>Thumbnail</th>
-              <th>First name</th>
-              <th>Last name</th>
-              <th>Email</th>
-              <th>Gender</th>
-              <th>Nationality</th>
-            </tr>
-          </thead>
-          <tbody>
-            {/* Looping through all the users from the API  */}
-            {this.state.users.length ? (
-              this.state.people.map((users, i) => {
-                return (
-                  <tr key={i}>
-                    <td>
-                      <img src={users.picture.thumbnail} alt="thumbnail" />
-                    </td>
-                    <td>{users.name.first}</td>
-                    <td>{users.name.last}</td>
-                    <td>{users.email}</td>
-                    <td>{users.gender}</td>
-                    <td>{users.nat}</td>
-                  </tr>
-                );
-              })
-            ) : (
-              <h3 className="no-result-message mt-5">No result to display!</h3>
-            )}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <thead>
+      <tr>
+        <th>
+          <img alt={props.firstName} src={props.picture} />
+        </th>
+        <td>{props.firstName}</td>
+        <td>{props.lastName}</td>
+        <td>{props.email}</td>
+        <td>{props.phone}</td>
+        <td>{props.city}</td>
+      </tr>
+    </thead>
   );
 }
 
