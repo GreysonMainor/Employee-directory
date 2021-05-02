2d56bc58c5505e2a358c96b6316804824cb8af7c
Greyson Mainor <gbmainor98@outlook.com>
2021-05-01 20:58:35 -0400

    added Col folder.

--- employees-directory/src/components/App.js
+++ employees-directory/src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import EmployeeCard from './components/EmployeeCard';
+import SearchForm from './components/SearchForm';
+import Wrapper from './components/Wrapper';
+import Col from './components/Col';
 import API from './utils/API';
 import './App.css';
 
 class App extends React.Component {
-  state = { employees: [] };
+  state = { employees: [], search: '' };
 
   componentDidMount() {
     API.search()
@@ -17,54 +20,86 @@ class App extends React.Component {
             picture: e.picture.large,
             email: e.email,
             phone: e.phone,
-            dob: e.age,
+            city: e.location.city,
             key: i,
           })),
         });
       })
       .catch((err) => console.log(err));
-  }
+  };
 
-  getEmployees = () => {
-    API.search()
-      .then((res) => this.setState({ result: res.data }))
-      .catch((err) => console.log(err));
+  refreshPage() {
+    window.location.reload(false);
+  };
+
+  searchEmployee = (filter) => {
+    console.log('Search', filter);
+    const filteredList = this.state.employees.filter((employee) => {
+      let values = Object.values(employee).join('').toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    this.setState({ employees: filteredList });
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log('Handle ', this.state.search);
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Button Clicked', this.state.search, e);
+    this.searchEmployee(this.state.search);
   };
 
   render() {
     return (
-      <div className="container">
-        <div>
-          <div>
-            <h2>Employee Directory</h2>
+      <Wrapper>
+        <div className="container">
+          <div className="row">
+            <Col size="md-4">
+              <h2>Employee Directory</h2>
+              <SearchForm
+                value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+              />
+            </Col>
+          </div>
+
+          <div className="row">
+            <Col size="md-12">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Photo</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>City</th>
+                  </tr>
+                </thead>
+                {[...this.state.employees].map((item) => (
+                  <EmployeeCard
+                    picture={item.picture}
+                    firstName={item.firstName}
+                    lastName={item.lastName}
+                    email={item.email}
+                    phone={item.phone}
+                    city={item.city}
+                    key={item.key}
+                  />
+                ))};
+              </table>
+            </Col>
           </div>
         </div>
-        <div>
-          <div></div>
-        </div>
-        <div>
-          <table className="table">
-            <tr>
-              <th>Photo</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Email</th>
-              <th>Phone</th>
-            </tr>
-
-            {[...this.state.employees].map((item) => (
-              <EmployeeCard
-                picture={item.picture}
-                firstName={item.firstName}
-                lastName={item.lastName}
-                email={item.email}
-                phone={item.phone}
-                key={item.key}
-              />
-            ))}
-          </table>
-        </div>
-      </div>
+      </Wrapper>
     );
-  }
-}
+  };
+};
+
+export default App;
